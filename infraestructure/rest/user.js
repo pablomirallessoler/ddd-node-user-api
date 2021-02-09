f7d33a38d63a7d1575f16fbf0303aaff36511979
Pablo Miralles <pablomirallessoler@gmail.com>
2021-02-09 12:22:26 +0100

    adds commands and responses to user rest

--- infraestructure/rest/user.js
+++ infraestructure/rest/user.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const authValidator = require('./middlewares/auth-validator');
+const LoginCommand = require('../../application/user/commands/login');
+const DeleteUserCommand = require('../../application/user/commands/delete');
+const RegisterUserCommand = require('../../application/user/commands/register');
+const UpdateUserCommand = require('../../application/user/commands/update');
+
+const LoginResponse = require('../../application/user/responses/login');
+
 const container = require('../../container');
 const registerUser = container.resolve('registerUser');
 const updateUser = container.resolve('updateUser');
@@ -9,7 +16,18 @@ const loginUser = container.resolve('loginUser');
 
 router.post('/users', async(req, res, next) => {
     try {
-        await registerUser.register(req.body);
+        const command = new RegisterUserCommand({
+            id: req.body.id,
+            password: req.body.password,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            phone: req.body.phone,
+            email: req.body.email,
+            country: req.body.country,
+            postalCode: req.body.postalCode
+        });
+
+        await registerUser.register(command);
 
         return res.status(201).send();
     } catch (ex) {
@@ -20,7 +38,18 @@ router.post('/users', async(req, res, next) => {
 router.put('/users/:id', authValidator, async(req, res, next) => {
     try {
         const id = req.params.id;
-        await updateUser.update({ id, ...req.body });
+
+        const command = new UpdateUserCommand({
+            id,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            phone: req.body.phone,
+            email: req.body.email,
+            country: req.body.country,
+            postalCode: req.body.postalCode
+        });
+
+        await updateUser.update(command);
 
         return res.status(204).send();
     } catch (ex) {
@@ -31,8 +60,9 @@ router.put('/users/:id', authValidator, async(req, res, next) => {
 router.delete('/users/:id', authValidator, async(req, res, next) => {
     try {
         const id = req.params.id;
-        await deleteUser.delete({ id });
+        const command = new DeleteUserCommand({ id });
 
+        await deleteUser.delete(command);
         return res.status(204).send();
     } catch (ex) {
         next(ex);
@@ -41,10 +71,12 @@ router.delete('/users/:id', authValidator, async(req, res, next) => {
 
 router.post('/login', async(req, res, next) => {
     try {
-        const id = req.params.id;
-        const token = await loginUser.login(req.body);
+        const command = new LoginCommand({ email: req.body.email, password: req.body.password });
 
-        return res.status(200).send({ token });
+        const token = await loginUser.login(command);
+        const response = new LoginResponse({ token });
+
+        return res.status(200).send(response);
     } catch (ex) {
         next(ex);
     }
